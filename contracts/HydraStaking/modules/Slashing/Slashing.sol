02425070f4279fdbbd390df7bbaf4213ade1968f
Amit22antier <amit.kumar22@antiersolutions.com>
2025-07-02 15:13:31 +0530

    WIP: Added slashing contract

--- contracts/HydraStaking/modules/Slashing/Slashing.sol
+++ contracts/HydraStaking/modules/Slashing/Slashing.sol
@@ -4,7 +4,10 @@ pragma solidity 0.8.17;
 import {ISlashing} from "./ISlashing.sol";
 import {System} from "../../../common/System/System.sol";
 import {Unauthorized} from "../../../common/Errors.sol";
-import {Staking} from "../../Staking.sol";
+
+interface IInspector {
+    function slashValidator(address validator, string calldata reason) external;
+}
 
 contract Slashing is ISlashing, System {
     // Mapping to track slashed amounts per validator
@@ -12,34 +15,25 @@ contract Slashing is ISlashing, System {
     // Mapping to track if a validator is slashed
     mapping(address => bool) private _isSlashed;
 
-    // Reference to the staking contract
-    Staking public immutable STAKING_CONTRACT;
+    // Reference to the HydraChain contract (Inspector module)
+    address public immutable HYDRA_CHAIN_CONTRACT;
 
-    constructor(address _stakingContract) {
-        STAKING_CONTRACT = Staking(_stakingContract);
+    constructor(address _hydraChainContract) {
+        HYDRA_CHAIN_CONTRACT = _hydraChainContract;
     }
 
-    /// @inheritdoc ISlashing
+    /**
+     * @notice Called by the system to slash a validator for double-signing.
+     * @dev On-chain evidence verification is omitted; assumes consensus nodes have already verified.
+     * @param validator Address of the validator to be slashed
+     * @param amount Amount to slash
+     * @param reason Reason for slashing (string)
+     */
     function slashValidator(address validator, uint256 amount, string calldata reason) external onlySystemCall {
-        // Can only slash active validators
-        if (STAKING_CONTRACT.stakeOf(validator) == 0) {
-            revert Unauthorized("NOT_ACTIVE_VALIDATOR");
-        }
-
-        // Can't slash more than the validator's stake
-        uint256 currentStake = STAKING_CONTRACT.stakeOf(validator);
-        if (amount > currentStake) {
-            amount = currentStake;
-        }
-
-        // Update slashed amount and status
-        _slashedAmounts[validator] += amount;
-        _isSlashed[validator] = true;
-
-        // Call the staking contract's internal unstake function
-        // This requires the staking contract to expose an internal unstake function
-        STAKING_CONTRACT.unstakeFor(validator, amount);
-        
+        require(validator != address(0), "Invalid validator address");
+        require(amount > 0, "Amount must be positive");
+        // Notify Inspector module on HydraChain
+        IInspector(HYDRA_CHAIN_CONTRACT).slashValidator(validator, reason);
         emit ValidatorSlashed(validator, amount, reason);
     }
 
